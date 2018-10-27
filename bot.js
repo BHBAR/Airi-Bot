2bb2c1191becf8262de97c5e881fe3ec66ec7f7d
BHBAR <44456030+BHBAR@users.noreply.github.com>
2018-10-27 01:56:29 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-      if (message.content === '!nep_ok') {
+      if (message.content === '!nep ok') {
         message.channel.send('https://stickershop.line-scdn.net/stickershop/v1/product/1702446/LINEStorePC/main@2x.png;compress=true'); 
       }
       else if (message.content === '!nepu') {
         message.channel.send('https://stickershop.line-scdn.net/stickershop/v1/sticker/22759784/ANDROID/sticker.png;compress=true'); 
       }
-      else if (message.content === '!see_ya') {
+      else if (message.content === '!see ya') {
         message.channel.send('https://stickershop.line-scdn.net/stickershop/v1/sticker/22759815/ANDROID/sticker.png;compress=true'); 
       }
       else if (message.content === '!welcome') {
