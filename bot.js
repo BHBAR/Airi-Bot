3def898f273fed683aef0bfabfb967aa9d4a00f6
BHBAR <44456030+BHBAR@users.noreply.github.com>
2018-10-27 01:47:41 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,9 @@ client.on('message', message => {
       if (message.content === '!nep_ok') {
         message.channel.send('https://stickershop.line-scdn.net/stickershop/v1/product/1702446/LINEStorePC/main@2x.png;compress=true'); 
       }
+      else if (message.content === '!nepu') {
+        message.channel.send('https://stickershop.line-scdn.net/stickershop/v1/sticker/22759784/ANDROID/sticker.png;compress=true'); 
+      }
     });
 
 // THIS MUST BE THIS WAY
