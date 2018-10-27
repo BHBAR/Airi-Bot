bb39041c5d43ddac81a9f9fdd7a26577047b5903
BHBAR <44456030+BHBAR@users.noreply.github.com>
2018-10-27 01:37:26 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
       if (message.content === '!nep_ok') {
         message.reply('https://stickershop.line-scdn.net/stickershop/v1/product/1702446/LINEStorePC/main@2x.png;compress=true'); 
       }
+client.on('message', message => {
+      if (message.content === '!ping') {
+        message.reply('pong'); 
+      }
     });
 
 // THIS MUST BE THIS WAY
