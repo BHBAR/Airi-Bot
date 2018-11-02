73e418fe125d91d9df6e7087c4b4ea25d5ca4297
BHBAR <44456030+BHBAR@users.noreply.github.com>
2018-11-02 16:41:32 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,9 @@ client.on('message', message => {
       else if (message.content === '!afraid') {
         message.channel.send('https://stickershop.line-scdn.net/stickershop/v1/sticker/8436089/ANDROID/sticker.png;compress=true'); 
       }
+      else if (message.content === '!waifu') {
+        message.channel.send('https://thumbs.gfycat.com/UnlawfulWarmheartedAruanas-size_restricted.gif'); 
+      }
     });
 
 // THIS MUST BE THIS WAY
